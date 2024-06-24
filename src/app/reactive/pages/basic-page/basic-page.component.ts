64ddc3ef1d4c7ab9b35a08f8b50f0d992b330810
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-06-24 13:03:34 -0500

    feat: :sparkles: Implementación de valores por defecto y reset form

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 0,
+  inStorage: 0,
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
   // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl(''),
@@ -15,18 +22,27 @@ export class BasicPageComponent {
   // });
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [ Validators.required, Validators.minLength(3) ]],
-    price: [0, [ Validators.required, Validators.min(0) ]],
-    inStorage: [0, [ Validators.required, Validators.min(0) ]],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
   constructor(private fb: FormBuilder) { }
 
+  ngOnInit(): void {
+    this.myForm.reset(rtx5090)
+  }
+
   onSave(): void {
 
-    if(this.myForm.invalid) return;
+    if (this.myForm.invalid) return;
 
     console.log(this.myForm.value)
+
+    this.myForm.reset({
+      price: 0,
+      inStorage: 10
+    });
   }
 
 }
