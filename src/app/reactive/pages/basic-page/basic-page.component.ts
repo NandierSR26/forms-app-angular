417a08c38ccc7981123354bcfe4c6388b4a3b04c
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-06-24 13:28:39 -0500

    feat: :sparkles: Optimización de mensajes de error en formulario

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -30,14 +30,42 @@ export class BasicPageComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.myForm.reset(rtx5090)
+    // this.myForm.reset(rtx5090)
+  }
+
+
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+          break;
+
+        case 'minlength':
+          return `Minimo ${errors['minlength'].requiredLength} caracteres`;
+          break;
+      }
+    }
+
+    return ''
   }
 
   onSave(): void {
 
-    if (this.myForm.invalid) return;
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    };
 
-    console.log(this.myForm.value)
+    // console.log(this.myForm.value);
 
     this.myForm.reset({
       price: 0,
