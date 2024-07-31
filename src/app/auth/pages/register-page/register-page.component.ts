273c685bd7df3d3fcd913fb5856f24b9018a6e1d
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-07-31 13:59:54 -0500

    feat: :white_check_mark: Se agrego validador de email asincrono

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidatorsService } from 'src/app/shared/service/validators.service';
+import { EmailValidator } from 'src/app/shared/validators/email-validator.service';
 // import * as customValidators from 'src/app/shared/validators/validators';
 
 @Component({
@@ -10,8 +11,16 @@ import { ValidatorsService } from 'src/app/shared/service/validators.service';
 export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [ Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern ) ]],
-    email: ['', [Validators.required, Validators.pattern( this.validatorsService.emailPattern ) ]],
+    name: [
+      '', 
+      [ Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern ) ]
+    ],
+    email: [
+      '', 
+      [Validators.required, Validators.pattern( this.validatorsService.emailPattern ) ],
+      // [ new EmailValidator() ]
+      [ this.emailValidator ]
+    ],
     username: [
       '', 
       [Validators.required, this.validatorsService.cantBeStrider]
@@ -22,7 +31,8 @@ export class RegisterPageComponent {
 
   constructor(
     private fb: FormBuilder,
-    private validatorsService: ValidatorsService
+    private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidator
   ) {}
 
   isValidField(field: string) {
